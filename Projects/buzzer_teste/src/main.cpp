ea4be39b286660cfd14bb3cc601eacc6bdc8f23a
Skinhas <lucassantosmonteiro010@gmail.com>
2026-06-15 11:10:34 -0300

    10

---
+++ Projects/buzzer_teste/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+// put function declarations here:
+
+
+void setup() {
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  
+  digitalWrite(13,HIGH);
+  delay(5000);
+  digitalWrite(13,LOW);
+  delay(5000);
+
+}
+
